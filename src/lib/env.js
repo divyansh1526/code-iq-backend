0c0e432681a542cb82282edd3ac738834fa682c8
Divyansh Verma <divyansh1526k@gmail.com>
2026-01-19 19:27:24 +0530

    changes

--- src/lib/env.js
+++ src/lib/env.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config({quiet: true});
 
 export const ENV = {
-    PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     CLIENT_URL: process.env.CLIENT_URL,
--- src/server.js
+++ src/server.js
@@ -1,46 +1,38 @@
 import express from 'express';
 import cors from 'cors';
-import {ENV} from './lib/env.js';
 import path from 'path';
-import { connectDB } from './lib/db.js';
-import {serve} from 'inngest/express';
-import {inngest, functions} from './lib/inngest.js';
+import { serve } from 'inngest/express';
 
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+import { inngest, functions } from './lib/inngest.js';
 
 const app = express();
-const PORT = ENV.PORT;
-const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({origin:ENV.CLIENT_URL, credentials: true}));
+app.use(cors({
+  origin: ENV.CLIENT_URL,
+  credentials: true
+}));
 
-app.use('/api/inngest', serve({client: inngest, functions}));
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 app.get('/api', (req, res) => {
-  res.json({msg: 'hello'});
+  res.json({ msg: 'hello' });
 });
 
-app.get('/book', (req, res) => {
-  res.json({msg: 'hi'});
-});
-
-if(ENV.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-  });
-}
-
-const startserver = async () => {
+const startServer = async () => {
   try {
     await connectDB();
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
 };
 
-startserver();
-
+startServer();
